edb193e3d6e3c607490f43eabd76c47f2c25f6f1
kimberlynunezdev <nunezvaldiglesiask@gmail.com>
2025-03-06 20:45:19 -0500

    tema funciones nuevo

---
+++ functions.py
@@ -0,0 +1,41 @@
+#como se declara una funcion?
+
+#definicion de funcion:
+def hacer_saludo():
+    #lo que hace la funcion es imprimir un mensaje
+    print("Hola, bienvenedio")
+
+hacer_saludo()
+
+#definicion de funcion con parametros
+def saludar_nombre(nombre, apellido):
+    #asignamos parametro al print
+    print(f"Hola, bienvenido {nombre} {apellido}")
+
+#llamamos ala funcion con el parametro
+saludar_nombre("Kimberly", "Nunez")
+
+
+#Ejercicio 2: Suma de dos números
+#Crea una función sumar(a, b) que reciba dos números y devuelva su suma.
+
+
+
+
+
+
+
+
+
+
+
+#Ejercicio 3: Área de un Rectángulo
+#Crea una función area_rectangulo(base, altura) que devuelva el área.
+
+
+
+
+
+
+#Ejercicio 4: Saludo Personalizado
+#Crea una función saludar(nombre="invitado") que reciba un nombre y lo use en un saludo.
